3c23e73a041199cff4cc72ae843d00227a924c2d
FeoluK <olufeolu@gmail.com>
2024-09-03 17:00:56 -0500

    Initial Commit

---
+++ SynchroSound/SynchroSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynchroSoundApp.swift
+//  SynchroSound
+//
+//  Created by Feolu Kolawole on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SynchroSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
